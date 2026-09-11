e1aa283f107caa0f3cc55bb2926ce04550169164
Steve Goldhaber <stevenng@met.no>
2026-09-11 15:33:50 +0200

    Add instantaneous versions of CO2 tracers Add TMCO2 diagnostic when there is no CO2 tracer

--- src/physics/cam/constituent_burden.F90
+++ src/physics/cam/constituent_burden.F90
@@ -9,6 +9,8 @@ module constituent_burden
 !-----------------------------------------------------------------------------------------
 
   use constituents, only: pcnst
+  use cam_history_support,  only: fieldname_len
+  use co2_cycle, only: ncnst, c_i, co2_transport
 
   implicit none
 
@@ -19,9 +21,8 @@ module constituent_burden
 
   private
 
-  character(len=18) :: burdennam(pcnst)     ! name of burden history variables
-
-  save
+  character(len=fieldname_len) :: burdennam(pcnst)      ! name of burden history variables
+  character(len=fieldname_len) :: burdennam_inst(ncnst) ! name of instantaneous burden history variables
 
 !=========================================================================================
 
@@ -34,13 +35,25 @@ subroutine constituent_burden_init
   use cam_history,   only: addfld, horiz_only
   use constituents,  only: cnst_name
 
-  integer :: m
+  integer :: mind
 
-  do m = 2, pcnst
-    burdennam(m) = 'TM'//cnst_name(m)
-    call addfld (burdennam(m), horiz_only, 'A', 'kg/m2', &
-                 trim(cnst_name(m)) // ' column burden')
+  do mind = 2, pcnst
+    burdennam(m) = 'TM'//trim(cnst_name(mind))
+    call addfld(burdennam(mind), horiz_only, 'A', 'kg/m2', &
+         trim(cnst_name(mind)) // ' column burden')
   end do
+  if (co2_transport()) then
+     do mind = 1, ncnst
+        burdennam_inst(mind) = 'TM'//trim(cnst_name(c_i(mind)))//'_INST'
+        call addfld(burdennam_inst(mind), horiz_only, 'A', 'kg/m2', &
+             trim(cnst_name(c_i(mind))) // ' column burden for instantaneous output')
+     end do
+  else
+     burdennam_inst(1) = 'TMCO2_INST'
+     call addfld(burdennam_inst(1), horiz_only, 'A', 'kg/m2', &
+          'CO2 column burden for instantaneous output')
+     burdennam_inst(2:) = ''
+  end if
 
 end subroutine constituent_burden_init
 
@@ -50,10 +63,11 @@ subroutine constituent_burden_comp(state)
 
   use physics_types, only: physics_state
   use shr_kind_mod,  only: r8 => shr_kind_r8
-  use constituents,  only: cnst_type
+  use constituents,  only: cnst_type, cnst_get_ind
   use ppgrid,        only: pcols
   use physconst,     only: rga
   use cam_history,   only: outfld, hist_fld_active
+  use chem_surfvals, only: chem_surfvals_get
 
 !-----------------------------------------------------------------------
 !
@@ -65,24 +79,49 @@ subroutine constituent_burden_comp(state)
 
   real(r8) :: ftem(pcols)      ! temporary workspace
 
-  integer :: m, lchnk, ncol
+  integer :: mind, lchnk, ncol, cind
 
   lchnk = state%lchnk
   ncol  = state%ncol
 
-  do m = 2, pcnst
-     if (.not. hist_fld_active(burdennam(m))) cycle
-     if (cnst_type(m) .eq. 'dry') then
-        ftem(:ncol) = sum(state%q(:ncol,:,m) * state%pdeldry(:ncol,:), dim=2) * rga
+  do mind = 2, pcnst
+     if (.not. hist_fld_active(burdennam(mind))) cycle
+     if (cnst_type(mind) .eq. 'dry') then
+        ftem(:ncol) = sum(state%q(:ncol,:,mind) * state%pdeldry(:ncol,:), dim=2) * rga
      else
-        ftem(:ncol) = sum(state%q(:ncol,:,m) * state%pdel(:ncol,:), dim=2) * rga
-     endif
-     call outfld (burdennam(m), ftem, pcols, lchnk)
+        ftem(:ncol) = sum(state%q(:ncol,:,mind) * state%pdel(:ncol,:), dim=2) * rga
+     end if
+     call outfld (burdennam(mind), ftem, pcols, lchnk)
   end do
+  ! Compute special instantaneous values
+  if (co2_transport()) then
+     do mind = 1, ncnst
+        if (.not. hist_fld_active(burdennam_inst(mind))) cycle
+        cind = c_i(mind)
+        if (cnst_type(cind) .eq. 'dry') then
+           ftem(:ncol) = sum(state%q(:ncol,:,cind) * state%pdeldry(:ncol,:), dim=2) * rga
+        else
+           ftem(:ncol) = sum(state%q(:ncol,:,cind) * state%pdel(:ncol,:), dim=2) * rga
+        end if
+        call outfld(burdennam(mind), ftem(:ncol), ncol, lchnk)
+     end do
+  else if (hist_fld_active(burdennam_inst(1))) then
+        call cnst_get_ind('CO2', cind, abort=.false.)
+        if (cind > 0) then
+           if (cnst_type(cind) .eq. 'dry') then
+              ftem(:ncol) = sum(state%q(:ncol,:,cind) * state%pdeldry(:ncol,:), dim=2) * rga
+           else
+              ftem(:ncol) = sum(state%q(:ncol,:,cind) * state%pdel(:ncol,:), dim=2) * rga
+           end if
+        else
+           ! There is no CO2 tracer, compute from co2mmr
+           ftem(:ncol) = chem_surfvals_get('CO2MMR', lchnk, ncol) * sum(state%pdeldry(:ncol,:), dim=2) * rga
+        end if
+        call outfld(burdennam(1), ftem(:ncol), ncol, lchnk)
+  end if
 
 end subroutine constituent_burden_comp
 
 !=========================================================================================
 
 end module constituent_burden
-
